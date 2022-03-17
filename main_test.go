eb016f03f04cb14bea15d703b535080aa800c386
Jim <jlambert@hashicorp.com>
2022-03-17 10:29:52 -0400

    It appears that zero values are not being scanned into the model...

--- main_test.go
+++ main_test.go
@@ -17,4 +17,34 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	// update name to an empty string
+	if err := DB.Model(&User{}).Where("id = ?", user.ID).Updates(map[string]interface{}{
+		"Name": "",
+	}).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	// now, we will search again for the user reusing the previous result
+	// variable
+	if err := DB.First(&result, user.ID).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	// the user name from the db should be an empty string, but it is still
+	// "jinzhu" from the previous search.... the empty string isn't being set in
+	// the model.
+	if result.Name != "" {
+		t.Errorf("Failed, name should be %q but it's %q", "", result.Name)
+	}
+
+	// Let's prove that the name is actually an empty string in the db by
+	// searching again with an new variable for the model
+	var newResult User
+	if err := DB.First(&newResult, user.ID).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if user.Name != "" {
+		t.Errorf("name should be set to %q but it's now: %q", "", user.Name)
+	}
+
 }
